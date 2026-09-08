437a9e9505f6db0f873f7924d3691cb1c44e8ab1
Lurk Alma <njrush001@gmail.com>
2026-09-08 13:05:17 +0300

    Added Repeat All & Shuffle Algorithms

--- alma_music_player.py
+++ alma_music_player.py
@@ -1,5 +1,5 @@
 # <============ IMPORTS ===============>
-import threading
+import threading, random, os
 from new_ui_updater import UIUpdates
 from alma_ui_builder import ProgramUI
 from new_player_engine import PlayerEngine
@@ -35,6 +35,7 @@ class ProgramPlaylists:
 		# ---
 		self.main_playlist = []      # -- Main playlist with track paths
 		self.main_basenames = []
+		self.shuffled_playlist = []
 		self.users_favourites = []   # -- Users favourite songs
 		# ---
 
@@ -55,6 +56,23 @@ class ProgramPlaylists:
 			self.main_basenames.append(track)
 			self.main_playlist.append(track_path)
 
+			try:
+				# -- Clamp btn 0 and max length
+				self.shuffled_playlist.insert(
+					random.randint(
+						0,
+						(len(self.shuffled_playlist) - 1)
+					),
+					track_path
+				)
+
+			except ValueError:
+				# -- Insert at pos 0
+				self.shuffled_playlist.insert(
+					0,
+					track_path
+				)
+
 		if added:
 			# -- Display added songs
 			self.root.after(
@@ -62,6 +80,106 @@ class ProgramPlaylists:
 				self.pub.mini_queue,
 				added
 			)
+
+		#<_end of the method_>
+
+	def repeat_all_mode_idx(self, hint: int) -> None:
+		''' Sets the next track index. Also runs few algorithms to ensure UI updates '''
+		# --
+		m_p = self.main_playlist
+		m_b = self.main_basenames
+
+		# -- Update current index (wrap-around incase we at the end)
+		prev_index: int = self.pyr.track_index
+		self.pyr.track_index = (self.pyr.track_index + hint) % len(m_p)
+
+		if ((self.pyr.track_index % 5) == 0) and (hint == 1):
+			# -- Build Mini queue
+
+			tracks: list[str] = m_b[self.pyr.track_index:]
+			start_point: int = m_b.index(tracks[0])
+
+			build_mini_queue: bool = True
+
+		elif (hint == -1) and ((prev_index % 5) == 0):
+			# -- Build Mini queue
+
+			# -- Check positions of indices
+			if self.pyr.track_index > prev_index:
+				# -- prev index is at 0, while track index may be at 15: find starting point
+				if ((self.pyr.track_index % 5) == 0):
+					# -- Should display only one item
+					tracks: list[str] = [m_b[self.pyr.track_index]]
+
+				else:
+					# -- find previous int divisible by 5
+					pos: int = self.pyr.track_index
+
+					while ((pos % 5) != 0):
+						# -- Decrease number
+						pos -= 1
+
+					# -- Collect items from pos to end
+					tracks: list[str] = m_b[pos:]
+
+			else:
+				# -- Normal queue
+
+				tracks: list[str] = m_b[(prev_index - 5):]
+
+			start_point: int = m_b.index(tracks[0])
+			build_mini_queue: bool = True
+
+		else:
+			# -- Don't Build
+			tracks: list = []
+			start_point: int = 0
+			build_mini_queue: bool = False
+
+		return tracks, start_point, build_mini_queue
+
+		#<_end of the method_>
+
+	def shuffle_mode_idx(self, hint: int) -> None:
+		''' Sets the next track index. Also runs few algorithms to ensure UI updates '''
+		# --
+		m_p = self.main_playlist
+		m_b = self.main_basenames
+		s_p = self.shuffled_playlist
+
+		# --
+		prev_index: int = self.pyr.track_index
+		shuff_idx: int = s_p.index(m_p[self.pyr.track_index])
+		self.pyr.track_index = m_p.index(s_p[((shuff_idx + hint) % len(s_p))])
+
+		# -- Build?
+		if (self.pyr.track_index % 5 == 0):
+			# -- Build mini queue
+			tracks: str = m_b[self.pyr.track_index:]
+			start_point: int = m_b.index(tracks[0])
+			build_mini_queue: bool = True
+
+		elif (int(str(self.pyr.track_index - prev_index).replace('-', ''))) > 4:
+			# -- Find start point
+			pos: int = self.pyr.track_index
+
+			# --
+			while (pos % 5 != 0):
+				# -- reduce pos
+				pos -= 1
+
+			# --
+			tracks: list[str] = m_b[pos:]
+			start_point: int = m_b.index(tracks[0])
+			build_mini_queue: bool = True
+
+		else:
+			# -- Next song in the same mini queue as the previous song
+			tracks: list = []
+			start_point: int = 0
+			build_mini_queue: bool = False
+
+		return tracks, start_point, build_mini_queue
 
 		#<_end of the method_>
 
@@ -136,14 +254,6 @@ class MusicApp(ProgramPlaylists):
 			object=loop_btn,
 			text=text
 		)
-
-		#<_end of the method_>
-
-	def update_progress(self) -> None:
-		'''
-		Displays progress ratio, time elapsed and remaining time.
-		'''
-		pass
 
 		#<_end of the method_>
 
